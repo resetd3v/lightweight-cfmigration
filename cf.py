74471d055f3df34b8a852e447e02336b343a6304
reset <36563646+resetd3v@users.noreply.github.com>
2024-05-02 09:52:34 +0000

    BR

--- cf.py
+++ cf.py
@@ -1,60 +1,70 @@
 import requests, discord, time
-from consts import consts
+from consts import consts, utils
 from discord.ext import commands, tasks
 from os import name as platform
 from inspect import stack
 
-async def cfswitch(ip):
+# ".	"
+async def cfcheck():
+    error = None
     timer = time.time()
-    # purge dns records
-    dnsresp = requests.get(f"https://api.cloudflare.com/client/v4/zones/{consts.cfZone}/dns_records?type=A", headers=consts.cfheadersLmao)
-    if dnsresp.status_code != 200: return (False, None, None, None)
-
-    for record in dnsresp.json()["result"]:
-        requests.delete(f"https://api.cloudflare.com/client/v4/zones/{consts.cfZone}/dns_records/{record['id']}", headers=consts.cfheadersLmao)
-
-    # get records replace placeholder with ip and upload
-    # requests.Request('POST', f"https://api.cloudflare.com/client/v4/zones/{cfzone}/dns_records/import", headers=uploadHeaders, files=upload).prepare().body.decode('utf8')
-    upload = {
-        "file" : ("confusing.dns.txt", open(f"{__file__.replace('__init__.py', '')}confusing.wtf.txt").read().replace("REPLACEHERE", ip), "text/plain"),
-        "proxied" : (None, "true")
-        }
-    uploadresp = requests.post(f"https://api.cloudflare.com/client/v4/zones/{consts.cfZone}/dns_records/import", headers=consts.cfheadersLmao, files=upload).json()
-
-    # uploadresp["timing"]["process_time"] doesnt fucking exist anymore
-    if uploadresp["success"]: return (uploadresp["success"], uploadresp["result"]["recs_added"], dnsresp.json()["result_info"]["count"], time.time() - timer)
-    else: return (False, None, None, None)
-
-
-@tasks.loop(minutes=30)
-async def changeCheck(auto=True):
     resp = requests.get("https://checkip.amazonaws.com/")
-    if resp.status_code != 200: return
-    respText = resp.text.replace("\n", "")
+    if resp.status_code != 200: return {"success" : False, "error" : "invalidCheckIP"}
+    # fuck u amazon
+    ip = resp.text.replace("\n", "")
 
-    # cant use r+ for some reason
-    #delimter = "/" if platform != "nt" else "\\"
-    with open(f"{__file__.replace(f'__init__.py', '')}lmao.txt", "r") as fileLmao:
-        iper = fileLmao.read()
-        fileLmao.close()
+    dnsresp = requests.get(f"https://api.cloudflare.com/client/v4/zones/{consts.cfZone}/dns_records?type=A", headers=consts.cfheadersLmao)
+    if dnsresp.status_code != 200: error = "getDNS"
 
-    if iper == respText: return
+    # idk if including the open in the ternary creates a new instance every iteration or not
+    try:
+        with open(f"{__file__.replace('__init__.py', '')}records.txt") as file:
+            data = file.read()
+    except Exception as e:
+        error = "file"
 
-    with open(f"{__file__.replace(f'__init__.py', '')}lmao.txt", "w") as fileLmao:
-        fileLmao.truncate(0)
-        fileLmao.write(respText)
-        fileLmao.close()
-    
-    if auto:
-        success, recordCount, origdnscount, time = await cfswitch(respText)
-        dnsmode = "switched" if origdnscount > 0 else "created"
-        # only print second part if successful
-        cfswitchText = f"{f'cf returned {success}'} {f'| {dnsmode} {recordCount} records in {time}s' if success else ''}"
+    recordsList = [record for record in dnsresp.json()["result"] if record["name"] in data]
 
-    iptext = f"{iper.split('.')[0]}.\\*.\\*.* -> {respText.split('.')[0]}.\\*.\\*.*"
+    originalCount, successCount, failCount = 0, 0, 0
+    if not error:
+        for record in recordsList:
+            originalCount += 1
+            if record["content"] != ip:
+                newRecord = record
+                newRecord["content"] = ip
+                updateResp = requests.patch(f"https://api.cloudflare.com/client/v4/zones/{consts.cfZone}/dns_records/{record['id']}", headers=consts.cfheadersLmao, json=newRecord)
+                if updateResp.status_code == 200 and updateResp.json()["success"]: successCount += 1
+                else: failCount += 1
+
+        if successCount == 0 and failCount == 0: error = "ipValid"
+
+    if error: return {"success" : False, "error" : error}
+
+    dnsresp = dnsresp.json()
+    return {
+        "success" : True,
+        "recordCount" : successCount,
+        "failedCount" : failCount,
+        "originalCount" : originalCount,
+        "totalCount" : dnsresp["result_info"]["count"],
+        "timeElapsed" : time.time() - timer
+        }
+
+
+
+@tasks.loop(minutes=5)
+async def cfPrint():
+    resp = await cfcheck()
+    if not resp["success"]:
+        if resp["error"] == "ipValid":
+            if cfPrint.current_loop == 0: await utils.LOGGER.log(stack()[0][3], resp["error"], "debug")
+            return
+        else: await utils.LOGGER.log(stack()[0][3], f'cf returned {resp["error"]}', "ERROR")
+
     embed = discord.Embed(title="ermmm", color=0x0084d1)
     # can do {iptext + f' | cf returned {success}} but cuz the second part we cant atleast on python ver below 11
-    embed.description = f"{iptext} | {cfswitchText if auto else ''}"
-    print(f"[{stack()[0][3].upper()}] new ip {iptext} | {cfswitchText if auto else ''}")
+    embed.description = f'{resp["recordCount"]}/{resp["originalCount"]} switched in {resp["timeElapsed"]}' if resp["success"] else resp["error"]
+
+    utils.LOGGER.log(stack()[0][3], embed.description)
     channel = consts.bot.get_channel(consts.vpschannel)
     await channel.send(embed=embed)
